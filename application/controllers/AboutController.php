01082cc46415f7bf54546c4eb733adee5dab4086
MichaelTerpelivets <mterpelyvets@gmail.com>
2018-09-02 10:48:55 +0300

    'about'

---
+++ application/controllers/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael Terpelyvets
+ * Date: 02.09.2018
+ * Time: 10:35
+ */
+
+class AboutController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ application/controllers/ContactController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael Terpelyvets
+ * Date: 02.09.2018
+ * Time: 10:36
+ */
+
+class ContactController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
