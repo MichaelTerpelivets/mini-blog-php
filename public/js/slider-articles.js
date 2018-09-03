176938c00344ce515d143e5fcd5fe7d35c4d2af4
MichaelTerpelivets <mterpelyvets@gmail.com>
2018-09-03 14:43:46 +0300

    'review'

--- public/js/slider-articles.js
+++ public/js/slider-articles.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $("#testimonial-slider").owlCarousel({
         items: 1,
-        autoplayTimeout: 1000,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 5000,
         loop: true,
-        transitionStyle: "fade",
         nav: true,
         navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
-        autoPlay: true
+        autoplay: true
     });
 });
